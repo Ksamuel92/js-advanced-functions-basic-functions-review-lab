1b92fc86d3a9cf9b504e9e2425bb3a15c56ff86d
Kyle Samuel <ksamuel92@gmail.com>
2021-09-08 17:55:18 -0400

    commit tree 044d40ecac0e17d9d046514f5a27ee3632229562

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activityString = "roller-skate"){
+  return `This Saturday, I want to ${activityString}!`
+}
+
+const mondayWork = (activityString = "go to the office") => {
+return `This Monday, I will ${activityString}.`
+}
+
+function wrapAdjective(string = "*"){
+  return function(adjective = "special"){
+    return `You are ${string}${adjective}${string}!`;
+  }
+}
+
+const Calculator = {}
